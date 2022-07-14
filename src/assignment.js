134a35e95894c351a3dbf3838c989d12417574ff
Elkanah <81132079+Lumsky@users.noreply.github.com>
2022-07-14 21:10:01 +0000

    and now 3

--- src/assignment.js
+++ src/assignment.js
@@ -62,18 +62,19 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-// function celsiusToFahrenheit(arrayOfNumbers) {
-//   var fahrenheit = (arrayOfNumbers * 9/5) + 32;
-//     return Math.trunc(fahrenheit);
 
-// var arrayOfNumbers = [2, 4, 5, 7, 6, 9, 10]
-// var tempInFahrenheit = arrayOfNumbers.map(celsiusToFahrenheit);
+function celsiusToFahrenheit(arrayOfNumbers) {
+  var fahrenheit = (arrayOfNumbers * 9/5) + 32;
+    return Math.trunc(fahrenheit);
 
-// console.log(tempInFahrenheit)
-     // return arrayOfNumbers;
-//}
+var arrayOfNumbers = [2, 4, 5, 7, 6, 9, 10]
+var tempInFahrenheit = arrayOfNumbers.map(celsiusToFahrenheit);
 
-//assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+console.log(tempInFahrenheit)
+     return arrayOfNumbers;
+}
+
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 // ========================
 // DO NOT EDIT THIS BLOCK
