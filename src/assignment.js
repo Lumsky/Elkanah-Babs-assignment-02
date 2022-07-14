b2a0a2f4b0a641906d8c23505c921732a4f25701
Elkanah <81132079+Lumsky@users.noreply.github.com>
2022-07-14 19:00:19 +0000

    Commented out the array

--- src/assignment.js
+++ src/assignment.js
@@ -20,7 +20,7 @@ function sumOfNumbers(arrayOfNumbers) {
     sumOfNumbers += arrayOfNumbers[i];
     return sumOfNumbers;
 }
-let arrayOfNumbers = [2, 4, 5, 7, 6, 9, 10]
+// let arrayOfNumbers = [2, 4, 5, 7, 6, 9, 10]
   console.log(sumOfNumbers(arrayOfNumbers))
 
   // return arrayOfNumbers.length;
