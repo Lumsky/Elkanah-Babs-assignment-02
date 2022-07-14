b262daef657cd00f64b3db91d8d83889cb34f474
Elkanah <81132079+Lumsky@users.noreply.github.com>
2022-07-14 09:58:24 +0000

    deleted console.log code.

--- src/assignment.js
+++ src/assignment.js
@@ -19,7 +19,6 @@ function sumOfNumbers(arrayOfNumbers) {
     sumOfNumbers += arrayOfNumbers[i];
     return sumOfNumbers;
 
-let arrayOfNumbers = [2, 4, 5, 7, 6, 9, 10]
   console.log(sumOfNumbers(arrayOfNumbers))
     return arrayOfNumbers.length;
 }
